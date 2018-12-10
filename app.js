ffe2f1c8ffa0eff6bcf6fd23a488e484a053a921
Stefaan Seys <stefaan.seys@onespan.com>
2018-12-10 17:52:27 +0100

    read token

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const morgan = require('morgan');
 const express = require('express');
+var fs = require('fs')
 
 var app = express();
 
@@ -7,7 +8,13 @@ var app = express();
 var server_port = process.env.VAULTKUBEAUTHDEMO_SERVICE_PORT || 8080
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   var resp = 'Read failed';
+   try {
+      resp = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
+   } catch (err) {
+      resp = err.toString();
+   }   
+   res.send(resp);
 })
 
 var server = app.listen(server_port, function () {
