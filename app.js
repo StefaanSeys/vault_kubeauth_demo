a0f3a725d632c67b553ae8172dbe5feebad2dc32
Stefaan Seys <stefaan.seys@onespan.com>
2018-12-10 17:22:03 +0100

    envs

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require('express');
 var app = express();
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.VAULTKUBEAUTHDEMO_SERVICE_PORT || 8080
+var server_ip_address = process.env.VAULTKUBEAUTHDEMO_SERVICE_HOST || '127.0.0.1'
 
 app.get('/', function (req, res) {
    res.send('Hello World');
