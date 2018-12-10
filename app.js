fff929558f840e4e2fb76ddb327db89b765b0d54
Stefaan Seys <stefaan.seys@onespan.com>
2018-12-10 17:25:16 +0100

    no env

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ var app = express();
 
 
 var server_port = process.env.VAULTKUBEAUTHDEMO_SERVICE_PORT || 8080
-var server_ip_address = process.env.VAULTKUBEAUTHDEMO_SERVICE_HOST || '127.0.0.1'
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(server_port, server_ip_address, function () {
+var server = app.listen(server_port, function () {
    var host = server.address().address
    var port = server.address().port
    
