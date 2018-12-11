17c795dfa9812a7bd52abdd5350c4e86421b7dfe
Stefaan Seys <stefaan.seys@onespan.com>
2018-12-11 13:17:21 +0100

    first axios attempt

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const morgan = require('morgan');
 const express = require('express');
-var fs = require('fs')
+const fs = require('fs');
+const axios = require('axios');
 
-var app = express();
-
-
+// Setup our server port
 var server_port = process.env.VAULTKUBEAUTHDEMO_SERVICE_PORT || 8080
 
+// We read our Kubernetes service account token at the start
+let token = ""
+try {
+   token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
+} catch (err) {
+   console.log("ERROR: could not read token from the file system");
+}
+
+// Create our express instance
+var app = express();
+
+// Set up the one path we will listen on ("/")
 app.get('/', function (req, res) {
-   var resp = 'Read failed';
-   try {
-      resp = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8');
-   } catch (err) {
-      resp = err.toString();
-   }   
-   res.send(resp);
+
+   // Call the two vault APIs in a row
+   axios.post('https://localhost:8200/v1/auth/kubernetes/login', {
+      "jwt": token, 
+      "role": "simple-node-app"
+   })
+  .then(response => {
+      res.send(response);
+  })
+  .catch(error => {
+     console.log(error);
+      res.send("error");
+  });
+
+      
+   
 })
 
+// Start the server
 var server = app.listen(server_port, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+   var host = server.address().address;
+   var port = server.address().port;
+   console.log("Simple Node App listening at http://%s:%s", host, port)
 })
