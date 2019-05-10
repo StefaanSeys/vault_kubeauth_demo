1d4608aa19ba6871bf9bec0d7f142c09dfe240cb
Stefaan Seys <stefaan.seys@onespan.com>
2019-05-10 17:00:20 +0200

    update with new demo

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const fs = require('fs');
 const axios = require('axios');
 const https = require('https');
 
+// Setup our server port
+var server_port = process.env.VAULTKUBEAUTHDEMO_SERVICE_PORT || 8080;
+
+
+
 const instance = axios.create({
    httpsAgent: new https.Agent({  
      rejectUnauthorized: false
    })
  });
 
-// Setup our server port
-var server_port = process.env.VAULTKUBEAUTHDEMO_SERVICE_PORT || 8080;
-var vault_host = process.env.VAULT_SERVICE_HOST || 'localhost';
+
 
 // We read our Kubernetes service account token at the start
 let token = ""
@@ -28,21 +31,24 @@ var app = express();
 // Set up the one path we will listen on ("/")
 app.get('/', function (req, res) {
 
+   // Get the Vault host
+   var vault_host = process.env.VAULT_SERVICE_HOST || 'localhost';
+
    // Call the two vault APIs in a row
    instance.post('https://' + vault_host + ':8200/v1/auth/kubernetes/login', {
       "jwt": token, 
-      "role": "simple-node-app"
+      "role": "nodeapp-one"
    })
   .then(response => {
       let client_token = response.data.auth.client_token;
       let config = {
          headers: {'X-Vault-Token': client_token}
       };
-      return instance.get('https://' + vault_host + ':8200/v1/secret/simple-node-app', config);
+      return instance.get('https://' + vault_host + ':8200/v1/kv/nodeapp/one/stuff', config);
       
   })
   .then(response => {
-      res.send("Success! The password stored in Vault is \"" + response.data.data.password + "\"");
+      res.send("Success! The password stored in Vault is \"" + response.data.data.personal_secret + "\"");
    })
   .catch(error => {
       console.log(error);
